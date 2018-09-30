0130d763c87db46f2330e60a5aa70fcb23c4c7b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-30 19:42:22 +0000

    Deploy victoralvess/gs-vue to github.com/victoralvess/gs-vue.git:gh-pages

--- js/about.2e4c4b25.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("h1",[t._v("This is an about page")])])}],o={name:"about",methods:{track:function(){this.$ga.page("/about")}}},s=o,i=a("2877"),c=Object(i["a"])(s,n,u,!1,null,null,null);c.options.__file="About.vue";e["default"]=c.exports}}]);
-//# sourceMappingURL=about.2e4c4b25.js.map
